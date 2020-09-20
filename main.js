fc2ef9355982d7ba1d3e495c33a99a8e81048b0a
Guido Verna Russo <guidofrontend@gmail.com>
2020-09-20 03:17:57 -0300

    codigo formateado con prettier

--- main.js
+++ main.js
@@ -1,35 +1,27 @@
-const namesNodeList = document.querySelectorAll('.person > h2 > a')
-const namesArray = Array.from(namesNodeList)
+const namesNodeList = document.querySelectorAll(".person > h2 > a");
+const namesArray = Array.from(namesNodeList);
 
-const form = document.forms[0]
-const buscador = form.elements.search
+const form = document.forms[0];
+const buscador = form.elements.search;
 
+let mayusNames = namesArray.map((person) => person.innerText.toUpperCase());
 
-let mayusNames = namesArray.map( person => person.innerText.toUpperCase())
-
-
-buscador.oninput = function() {
-    for(name of mayusNames) {
-        if(!name.includes(buscador.value.toUpperCase())) {
-            namesNodeList.forEach(
-                function (currentValue) {
-                    if (currentValue.innerText.toUpperCase() === name) {
-                        currentValue.parentElement.parentElement.style.display = 'none'
-                    }
-                }
-            ,name)
+buscador.oninput = function () {
+  for (name of mayusNames) {
+    if (!name.includes(buscador.value.toUpperCase())) {
+      namesNodeList.forEach(function (currentValue) {
+        if (currentValue.innerText.toUpperCase() === name) {
+          currentValue.parentElement.parentElement.style.display = "none";
         }
-        else {
-            namesNodeList.forEach(
-                function (currentValue) {
-                    if (currentValue.innerText.toUpperCase() === name) {
-                        currentValue.parentElement.parentElement.style.display = ''
-                    }
-                }
-            ,name)
+      }, name);
+    } else {
+      namesNodeList.forEach(function (currentValue) {
+        if (currentValue.innerText.toUpperCase() === name) {
+          currentValue.parentElement.parentElement.style.display = "";
         }
+      }, name);
     }
-}
+  }
+};
 
-
-buscador.onchange = () => {buscador.value = ''}
+buscador.onchange = () => (buscador.value = "");
